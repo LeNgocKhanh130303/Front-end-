f936a5d12670eefcf05d3e2e73bf76ec10ebe3c4
LeNgocKhanh130303 <ngockhanh0869@gmail.com>
2024-06-06 12:52:24 +0700

    Update code font end

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import Cart from './cart/cart'
-// import NoCreateproduct from'./cart/Product'
-import StoreGame from'./Store/Storegame'
 import './cart/cart.css'
+import NoCreateproduct from'./cart/Product'
+import StoreGame from'./Store/Storegame'
 import './Store/noproduct.css'
+import './DeatailProduct/dratilproduct.css'
+import ProductDeltail from './DeatailProduct/deatil'
+import Login  from './Login/Login'
+
+
+import  Abouts from  './aboutus/about'
+import './aboutus/aboutus.css'
+
 function App() {
 
   return (
    <main>
-    <Cart></Cart>
+    {/* <Cart></Cart> */}
     {/* <NoCreateproduct></NoCreateproduct>                                        */}
     {/* <StoreGame></StoreGame>d/ */}
+    {/* <Abouts></Abouts> */}
+    {/* <ProductDeltail></ProductDeltail> */}
+    <Login></Login>
    </main>
    
    
